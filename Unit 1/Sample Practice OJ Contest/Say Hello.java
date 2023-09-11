99f1982e2d485cfc46bb45107e64f3fdde024252
ParveendThakur <parveend1234@gmail.com>
2023-09-11 20:06:08 +0530

    done simple OJ

--- Unit 1/Sample Practice OJ Contest/Say Hello.java
+++ Unit 1/Sample Practice OJ Contest/Say Hello.java
@@ -2,7 +2,6 @@
 // https://students.masaischool.com/assignments/31387?tab=assignmentDetails
 // https://oj.masaischool.com/contest/10055/problems
 
-
 public static void sayHello(String name){
         //write your code here
         System.out.print("Hello " + name);
