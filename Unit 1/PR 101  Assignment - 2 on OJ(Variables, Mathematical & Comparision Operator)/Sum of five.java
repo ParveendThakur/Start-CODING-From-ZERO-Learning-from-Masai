36f40e8f2667a294acee5b5afc8d48f35b889281
ParveendThakur <parveend1234@gmail.com>
2023-09-11 21:53:33 +0530

    done = Sum of five

---
+++ Unit 1/PR 101  Assignment - 2 on OJ(Variables, Mathematical & Comparision Operator)/Sum of five.java
@@ -0,0 +1,6 @@
+    public static void sumOfFive(int one,int two,int three,int four,int five){
+        //write your code here
+        int sum = one+two+three+four+five;
+        
+        System.out.println(sum);
+    }
