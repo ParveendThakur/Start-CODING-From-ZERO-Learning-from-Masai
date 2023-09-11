935cbfc3120376c626d1742262bbfcfeb9a175a0
ParveendThakur <parveend1234@gmail.com>
2023-09-11 22:30:19 +0530

    Double Two Triple One

---
+++ Unit 1/PR 101  Assignment - 2 on OJ(Variables, Mathematical & Comparision Operator)/Double Two Triple One.java
@@ -0,0 +1,11 @@
+    public static void doubleTwoTripleFour(int one,int two,int three,int four){
+        //write your code here
+        int a = one*2 ; 
+        int b =two*2; 
+        int c = three*3;
+        int d = four+4;
+        
+        int x = a+b+c+d;
+        
+        System.out.println(x);
+    }
