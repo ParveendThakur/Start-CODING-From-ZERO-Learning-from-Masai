c9d949ae8d916206faa98e09a23b44db6ca07deb
ParveendThakur <parveend1234@gmail.com>
2023-09-11 19:54:16 +0530

    Product

---
+++ Unit 1/Sample Practice OJ Contest/Find Product.java
@@ -0,0 +1,5 @@
+public static void findProduct(int one,int two,int three,int four,int five){
+        //write your code here
+        int product = one*two*three*four*five;
+        System.out.print(product);
+    }
