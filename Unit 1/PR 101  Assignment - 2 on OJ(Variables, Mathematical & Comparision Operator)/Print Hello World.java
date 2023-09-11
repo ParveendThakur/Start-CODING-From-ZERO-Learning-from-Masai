ebac191d9398470cd02ea6bf82a82a9af6eba1bf
ParveendThakur <parveend1234@gmail.com>
2023-09-11 20:40:08 +0530

    Print Hello World

--- Unit 1/PR 101  Assignment - 2 on OJ(Variables, Mathematical & Comparision Operator)/Print Hello World.java
+++ Unit 1/PR 101  Assignment - 2 on OJ(Variables, Mathematical & Comparision Operator)/Print Hello World.java
@@ -2,3 +2,11 @@
 // https://students.masaischool.com/assignments/31406?tab=assignmentDetails
 // link --->>> https://course.masaischool.com/assignments/31406
 // link OJ --->>> https://oj.masaischool.com/contest/10062/problems
+
+
+
+  public static void main(String[] args){
+    //write your code here
+    System.out.println("Hello World");
+  }
+
