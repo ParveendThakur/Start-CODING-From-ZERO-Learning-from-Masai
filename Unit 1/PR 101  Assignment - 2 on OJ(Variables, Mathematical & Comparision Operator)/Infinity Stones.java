235df8f6be44946f691ea15cb23e84bae2e601a8
ParveendThakur <parveend1234@gmail.com>
2023-09-12 00:14:21 +0530

    Infinity Stones

---
+++ Unit 1/PR 101  Assignment - 2 on OJ(Variables, Mathematical & Comparision Operator)/Infinity Stones.java
@@ -0,0 +1,5 @@
+  public static void infinityStones(int one,int two,int three,int four,int five,int six){
+    //write your code here
+    int x =+ one*1 + two*2 + three*3 + four*3 + five*2 + six*2;
+    System.out.println(x);
+  }
