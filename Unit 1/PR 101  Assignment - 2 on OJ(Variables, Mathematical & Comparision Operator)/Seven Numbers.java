20af44003b59e7960e8b5f147756c8b0bc90b129
ParveendThakur <parveend1234@gmail.com>
2023-09-11 23:07:50 +0530

    Seven Numbers

---
+++ Unit 1/PR 101  Assignment - 2 on OJ(Variables, Mathematical & Comparision Operator)/Seven Numbers.java
@@ -0,0 +1,7 @@
+public static void operationOnSevenNumber(int A,int B,int C,int D,int E,int F,int G){
+   //write your code here
+   int x = A+B+C;
+   int y = D+E+F+G;
+   
+   System.out.println(x*y);
+}
