f9b1a52dc01bba7e9111ab00e89fe3ef859d4939
ParveendThakur <parveend1234@gmail.com>
2023-09-11 21:50:00 +0530

    Find Product

---
+++ Unit 1/PR 101  Assignment - 2 on OJ(Variables, Mathematical & Comparision Operator)/Find Product.java
@@ -0,0 +1,6 @@
+    public static void productOfFive(int one,int two,int three,int four,int five){
+        //write your code here
+        int product = one*two*three*four*five;
+        
+        System.out.println(product);
+    }
