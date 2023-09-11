0d12169043cf211ab335f31b63adaae2a71af427
ParveendThakur <parveend1234@gmail.com>
2023-09-12 01:09:41 +0530

    Compare Seven Numbers

---
+++ Unit 1/PR 101  Assignment - 2 on OJ(Variables, Mathematical & Comparision Operator)/Compare Seven Numbers.java
@@ -0,0 +1,12 @@
+  //https://oj.masaischool.com/contest/10062/problem/12
+  public static void compareSevenNumbers(int a,int b,int c,int d,int e,int f,int g){
+    //write your code here
+    int sum1= (a+b)*c;
+    int sum2 = d+e+f+g;
+   // System.out.println(sum2);
+    if (sum1>sum2){
+       System.out.println("true");
+    }else{
+        System.out.println("false");
+  }
+  }
