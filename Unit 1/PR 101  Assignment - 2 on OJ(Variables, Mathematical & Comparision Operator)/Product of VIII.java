cc0434c6a8937bee964f20c8ceee625ab6fc74a0
ParveendThakur <parveend1234@gmail.com>
2023-09-11 21:57:04 +0530

    Product of VIII

---
+++ Unit 1/PR 101  Assignment - 2 on OJ(Variables, Mathematical & Comparision Operator)/Product of VIII.java
@@ -0,0 +1,5 @@
+    public static void productOfEight(int one,int two,int three,int four,int five,int six,int seven,int eight){
+        //write your code here
+        int product = one *two*three*four*five*six*seven*eight;
+        System.out.println(product);
+    }
