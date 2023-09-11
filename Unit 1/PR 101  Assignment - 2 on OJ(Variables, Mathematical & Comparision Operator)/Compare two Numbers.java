064ffaa7aae551ac29ccaf8a2529d55066b14d6a
ParveendThakur <parveend1234@gmail.com>
2023-09-12 00:46:23 +0530

    Compare two Numbers

---
+++ Unit 1/PR 101  Assignment - 2 on OJ(Variables, Mathematical & Comparision Operator)/Compare two Numbers.java
@@ -0,0 +1,18 @@
+// https://oj.masaischool.com/contest/10062/problem/10
+
+public static void compareAgain(int a,int b){
+  //write your code here
+  if (a>b){
+      System.out.println("true");
+  }else{
+      System.out.println("false");
+  }if (a<b){
+      System.out.println("true");
+  }else{
+      System.out.println("false");
+  }if (a==b){
+      System.out.println("true");
+  }else{
+      System.out.println("false");
+  }
+}
