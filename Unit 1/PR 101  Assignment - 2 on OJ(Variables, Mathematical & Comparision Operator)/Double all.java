d007a374d1757a041d86fc033d1a0be22a66f196
ParveendThakur <parveend1234@gmail.com>
2023-09-11 22:00:21 +0530

    Double all

---
+++ Unit 1/PR 101  Assignment - 2 on OJ(Variables, Mathematical & Comparision Operator)/Double all.java
@@ -0,0 +1,5 @@
+    public static void doubleAll(int one,int two,int three,int four,int five){
+        //write your code here
+        int sum = one+two+three+four+five;
+        System.out.println(sum*2);
+    }
