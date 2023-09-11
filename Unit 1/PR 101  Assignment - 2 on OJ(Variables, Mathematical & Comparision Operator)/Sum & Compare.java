a46a78cfbe0c5999215a5d171aabf5b0bea5b988
ParveendThakur <parveend1234@gmail.com>
2023-09-12 00:54:23 +0530

    Sum & Compare

---
+++ Unit 1/PR 101  Assignment - 2 on OJ(Variables, Mathematical & Comparision Operator)/Sum & Compare.java
@@ -0,0 +1,12 @@
+// https://oj.masaischool.com/contest/10062/problem/11
+public static void sumAndCompare(int one,int two,int three,int four,int five){
+  //write your code here
+  int sum1 = one+two+three;
+  int sum2 = four+five;
+  
+  if (sum1>sum2){
+      System.out.println("true");
+  }else{
+      System.out.println("false");
+  }
+}
