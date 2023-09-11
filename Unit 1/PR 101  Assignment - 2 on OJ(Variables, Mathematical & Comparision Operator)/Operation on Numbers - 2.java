767a1fb13e60f51d568c714e059eef2686e4579a
ParveendThakur <parveend1234@gmail.com>
2023-09-11 23:01:09 +0530

    Operation on Numbers - 2

---
+++ Unit 1/PR 101  Assignment - 2 on OJ(Variables, Mathematical & Comparision Operator)/Operation on Numbers - 2.java
@@ -0,0 +1,13 @@
+public static void operationOnTwoNumber(int n,int m){
+  //write your code here
+  
+  n = n*40+20-50;
+  
+  if (n>m){
+      System.out.println("Greater!");
+  }else if (n==m){
+      System.out.println("Equal!");
+  }else
+  System.out.println("Lesser!");
+ 
+}
