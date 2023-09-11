e37a42d02f85e5f793cdd54af797c31e5b9906ae
ParveendThakur <parveend1234@gmail.com>
2023-09-11 21:44:36 +0530

    Product of Six

---
+++ Unit 1/PR 101  Assignment - 2 on OJ(Variables, Mathematical & Comparision Operator)/Product of Six.java
@@ -0,0 +1,5 @@
+  public static void productOfSix(int one,int two,int three,int four,int five,int six){
+    //write your code here
+    int Product = one*two*three*four*five*six;
+    System.out.println(Product);
+  }
