c4fd858da9cee80b700c0e813feabceb7527a7b1
ParveendThakur <parveend1234@gmail.com>
2023-09-12 00:36:30 +0530

    Update & Compare

---
+++ Unit 1/PR 101  Assignment - 2 on OJ(Variables, Mathematical & Comparision Operator)/Update & Compare.java
@@ -0,0 +1,13 @@
+  public static void updateCompare(int num1,int num2,int num3){
+    //write your code here
+    if (num1>num2){
+        System.out.println("true");
+    }else {
+        System.out.println("false");
+    }
+    if (num1+num3 > num2){
+        System.out.println("true");
+    }else {
+        System.out.println("false");
+    }
+  }
