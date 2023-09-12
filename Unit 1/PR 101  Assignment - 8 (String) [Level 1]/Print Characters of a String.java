5cda64fe0dba0ddcd787e1059f4c575c7413677f
ParveendThakur <parveend1234@gmail.com>
2023-09-12 23:49:18 +0530

    Print Characters of a String

---
+++ Unit 1/PR 101  Assignment - 8 (String) [Level 1]/Print Characters of a String.java
@@ -0,0 +1,13 @@
+// link https://students.masaischool.com/assignments/31620?tab=assignmentDetails
+// link https://oj.masaischool.com/contest/10127/problems
+
+  public static void printChar(int n, String str){
+    //write your code here
+    int x = str.length();
+    for (int i = 0; i<x; i++){
+        char y = str.charAt(i);
+        System.out.println(y);
+    }
+    
+   // System.out.println(x);
+  }
