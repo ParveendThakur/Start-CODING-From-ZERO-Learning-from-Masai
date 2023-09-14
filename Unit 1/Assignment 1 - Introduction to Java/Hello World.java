dd7e93f45e93ad0ddf7223d12eb01adadf614a9d
ParveendThakur <parveend1234@gmail.com>
2023-09-15 01:44:39 +0530

    Print "Hello World!"

---
+++ Unit 1/Assignment 1 - Introduction to Java/Hello World.java
@@ -0,0 +1,9 @@
+/*
+Instructions
+Print "Hello World!" in the java console followed by "Hello World! How are you doing!!" in a different line
+*/
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
