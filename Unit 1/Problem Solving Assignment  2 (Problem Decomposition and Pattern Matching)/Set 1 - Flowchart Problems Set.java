0e6758d75880efb67fcf10ee2333a96d5c31de81
ParveendThakur <parveend1234@gmail.com>
2023-09-15 02:01:10 +0530

    Flowchart Problem Set

---
+++ Unit 1/Problem Solving Assignment  2 (Problem Decomposition and Pattern Matching)/Set 1 - Flowchart Problems Set.java
@@ -0,0 +1,790 @@
+# Flowchart Problem Set
+
+**Problem 1:** Identify the Flowchart Function
+
+You are given a flowchart as follows:
+
+```sql
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+Set sum to 0
+ |
+ v
+For i=1 to n
+ |   \
+ |    Set sum = sum + i
+ |   /
+ v
+End For
+ |
+ v
+Print sum
+ |
+ v
+End
+
+```
+
+What does this flowchart do?
+
+**Problem 2:** Identify the Error in the Flowchart
+
+You are given a flowchart as follows:
+
+```sql
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+If n mod 2 equals 0
+ |   \
+ |    Print "Number is odd"
+ |   /
+ v
+Else
+ |   \
+ |    Print "Number is even"
+ |   /
+ v
+End
+
+```
+
+What is the error in this flowchart?
+
+**Problem 3:** Complete the Flowchart
+
+You are given a partial flowchart:
+
+```sql
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+__________ (Fill this step)
+ |
+ v
+If n mod 2 equals 0
+ |   \
+ |    Print "Number is even"
+ |   /
+ v
+Else
+ |   \
+ |    Print "Number is odd"
+ |   /
+ v
+End
+
+```
+
+What would be a suitable step to fill the blank?
+
+**Problem 4:** Rearrange the Steps
+
+You are given a flowchart with steps in the wrong order:
+
+1. End
+2. Print "Number is even"
+3. Get integer n from user
+4. Start
+5. If n mod 2 equals 0
+6. Print "Number is odd"
+7. Else
+
+Rearrange these steps to make a valid flowchart.
+
+**Problem 5:** Flowchart Test Cases
+
+Given the following flowchart:
+
+```sql
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+If n > 0
+ |   \
+ |    Print "Number is positive"
+ |   /
+ v
+Else if n < 0
+ |   \
+ |    Print "Number is negative"
+ |   /
+ v
+Else
+ |   \
+ |    Print "Number is zero"
+ |   /
+ v
+End
+
+```
+
+What will be the output for the following test cases: n = -5, n = 0, n = 3?
+
+**Problem 6:** Identify the Flowchart Function
+
+You are given a flowchart as follows:
+
+```sql
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+Set factorial to 1
+ |
+ v
+For i=1 to n
+ |   \
+ |    Set factorial = factorial * i
+ |   /
+ v
+End For
+ |
+ v
+Print factorial
+ |
+ v
+End
+```
+
+What does this flowchart do?
+
+**Problem 7:** Identify the Error in the Flowchart
+
+You are given a flowchart as follows:
+
+```sql
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+For i=1 to n
+ |   \
+ |    Print i
+ |   /
+ v
+End For
+ |
+ v
+End
+
+```
+
+What is the error in this flowchart?
+
+**Problem 8:** Complete the Flowchart
+
+You are given a partial flowchart:
+
+```sql
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+Set sum to 0
+ |
+ v
+__________ (Fill this step)
+ |
+ v
+Print sum
+ |
+ v
+End
+
+```
+
+What would be a suitable step to fill the blank?
+
+**Problem 9:** Rearrange the Steps
+
+You are given a flowchart with steps in the wrong order:
+
+1. End
+2. Print factorial
+3. Get integer n from user
+4. Start
+5. For i=1 to n, set factorial = factorial * i
+6. Set factorial to 1
+7. End For
+
+Rearrange these steps to make a valid flowchart.
+
+**Problem 10:** Flowchart Test Cases
+
+Given the following flowchart:
+
+```sql
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+If n < 0
+ |   \
+ |    Print "Number is negative"
+ |   /
+ v
+Else
+ |   \
+ |    Print "Number is non-negative"
+ |   /
+ v
+End
+
+```
+
+What will be the output for the following test cases: n = -5, n = 0, n = 3?
+
+**Problem 21:** Identify the Flowchart Function
+
+You are given a flowchart as follows:
+
+```sql
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+Set sum_of_squares to 0
+ |
+ v
+For i=1 to n
+ |   \
+ |    Set sum_of_squares = sum_of_squares + i*i
+ |   /
+ v
+End For
+ |
+ v
+Print sum_of_squares
+ |
+ v
+End
+```
+
+What does this flowchart do?
+
+**Problem 22:** Identify the Error in the Flowchart
+
+You are given a flowchart as follows:
+
+```sql
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+If n < 0
+ |   \
+ |    Print "Number is positive"
+ |   /
+ v
+Else
+ |   \
+ |    Print "Number is negative"
+ |   /
+ v
+End
+
+```
+
+What is the error in this flowchart?
+
+**Problem 23:** Complete the Flowchart
+
+You are given a partial flowchart:
+
+```sql
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+__________ (Fill this step)
+ |
+ v
+If n mod 2 equals 0
+ |   \
+ |    Print "Number is even"
+ |   /
+ v
+Else
+ |   \
+ |    Print "Number is odd"
+ |   /
+ v
+End
+
+```
+
+What would be a suitable step to fill the blank?
+
+**Problem 24:** Rearrange the Steps
+
+You are given a flowchart with steps in the wrong order:
+
+1. End
+2. Print "Number is positive"
+3. Get integer n from user
+4. Start
+5. If n > 0
+6. Print "Number is negative"
+7. Else
+
+Rearrange these steps to make a valid flowchart.
+
+**Problem 25:** Flowchart Test Cases
+
+Given the following flowchart:
+
+```sql
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+If n > 0
+ |   \
+ |    Print "Number is positive"
+ |   /
+ v
+Else if n < 0
+ |   \
+ |    Print "Number is negative"
+ |   /
+ v
+Else
+ |   \
+ |    Print "Number is zero"
+ |   /
+ v
+End
+
+```
+
+What will be the output for the following test cases: n = -7, n = 0, n = 8?
+
+**Problem 26:** Identify the Flowchart Function
+
+You are given a flowchart as follows:
+
+```sql
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+Set square_sum to n * (n + 1) * (2n + 1) / 6
+ |
+ v
+Print square_sum
+ |
+ v
+End
+
+```
+
+What does this flowchart do?
+
+**Problem 27:** Identify the Error in the Flowchart
+
+You are given a flowchart as follows:
+
+```sql
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+If n > 0
+ |   \
+ |    Print "Number is negative"
+ |   /
+ v
+Else
+ |   \
+ |    Print "Number is positive"
+ |   /
+ v
+End
+
+```
+
+What is the error in this flowchart?
+
+**Problem 28:** Complete the Flowchart
+
+You are given a partial flowchart:
+
+```sql
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+Set sum to 0
+ |
+ v
+__________ (Fill this step)
+ |
+ v
+Print sum
+ |
+ v
+End
+
+```
+
+What would be a suitable step to fill the blank?
+
+**Problem 29:** Rearrange the Steps
+
+You are given a flowchart with steps in the wrong order:
+
+1. End
+2. Print sum_of_squares
+3. Get integer n from user
+4. Start
+5. For i=1 to n, set sum_of_squares = sum_of_squares + i*i
+6. Set sum_of_squares to 0
+7. End For
+
+Rearrange these steps to make a valid flowchart.
+
+**Problem 30:** Flowchart Test Cases
+
+Given the following flowchart:
+
+```sql
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+If n < 0
+ |   \
+ |    Print "Number is negative"
+ |   /
+ v
+Else
+ |   \
+ |    Print "Number is non-negative"
+ |   /
+ v
+End
+
+```
+
+What will be the output for the following test cases: n = -3, n = 0, n = 6?
+
+**Problem 31:** Identify the Flowchart Function
+
+You are given a flowchart as follows:
+
+```
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+Set factorial to 1
+ |
+ v
+For i=1 to n
+ |   \\
+ |    Set factorial = factorial * i
+ |   /
+ v
+End For
+ |
+ v
+Print factorial
+ |
+ v
+End
+
+```
+
+What does this flowchart do?
+
+**Problem 32:** Identify the Error in the Flowchart
+
+You are given a flowchart as follows:
+
+```
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+If n mod 2 equals 0
+ |   \\
+ |    Print "Number is odd"
+ |   /
+ v
+Else
+ |   \\
+ |    Print "Number is even"
+ |   /
+ v
+End
+
+```
+
+What is the error in this flowchart?
+
+**Problem 33:** Complete the Flowchart
+
+You are given a partial flowchart:
+
+```
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+__________ (Fill this step)
+ |
+ v
+If factorial equals 0
+ |   \\
+ |    Print "Number is zero"
+ |   /
+ v
+Else
+ |   \\
+ |    Print "Number is non-zero"
+ |   /
+ v
+End
+
+```
+
+What would be a suitable step to fill the blank?
+
+**Problem 34:** Rearrange the Steps
+
+You are given a flowchart with steps in the wrong order:
+
+1. End
+2. Print factorial
+3. Get integer n from user
+4. Start
+5. For i=1 to n, set factorial = factorial * i
+6. Set factorial to 1
+7. End For
+
+Rearrange these steps to make a valid flowchart.
+
+**Problem 35:** Flowchart Test Cases
+
+Given the following flowchart:
+
+```
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+If n > 0
+ |   \\
+ |    Print "Number is positive"
+ |   /
+ v
+Else if n < 0
+ |   \\
+ |    Print "Number is negative"
+ |   /
+ v
+Else
+ |   \\
+ |    Print "Number is zero"
+ |   /
+ v
+End
+
+```
+
+What will be the output for the following test cases: n = -1, n = 0, n = 1?
+
+**Problem 36:** Identify the Flowchart Function
+
+You are given a flowchart as follows:
+
+```
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+Set sum to 0
+ |
+ v
+For i=1 to n
+ |   \\
+ |    If i mod 3 equals 0 or i mod 5 equals 0
+ |   |   \\
+ |   |    Set sum = sum + i
+ |   |   /
+ |   v
+End For
+ |
+ v
+Print sum
+ |
+ v
+End
+
+```
+
+What does this flowchart do?
+
+**Problem 37:** Identify the Error in the Flowchart
+
+You are given a flowchart as follows:
+
+```
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+If n mod 2 equals 0
+ |   \\
+ |    Print "Number is even"
+ |   /
+ v
+Else
+ |   \\
+ |    Print "Number is odd"
+ |   /
+ v
+End
+
+```
+
+What is the error in this flowchart?
+
+**Problem 38:** Complete the Flowchart
+
+You are given a partial flowchart:
+
+```
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+Set sum to 0
+ |
+ v
+__________ (Fill this step)
+ |
+ v
+Print sum
+ |
+ v
+End
+
+```
+
+What would be a suitable step to fill the blank?
+
+**Problem 39:** Rearrange the Steps
+
+You are given a flowchart with steps in the wrong order:
+
+1. End
+2. Print sum
+3. Get integer n from user
+4. Start
+5. For i=1 to n, If i mod 3 equals 0 or i mod 5 equals 0, set sum = sum + i
+6. Set sum to 0
+7. End For
+
+Rearrange these steps to make a valid flowchart.
+
+**Problem 40:** Flowchart Test Cases
+
+Given the following flowchart:
+
+```
+Start
+ |
+ v
+Get integer n from user
+ |
+ v
+If n mod 2 equals 0
+ |   \\
+ |    Print "Number is even"
+ |   /
+ v
+Else
+ |   \\
+ |    Print "Number is odd"
+ |   /
+ v
+End
+
+```
+
+What will be the output for the following test cases: n = -2, n = 0, n = 3?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
