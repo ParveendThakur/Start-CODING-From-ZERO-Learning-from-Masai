6e12c4d6836e832e3a0eda382acc700091b24321
ParveendThakur <parveend1234@gmail.com>
2023-09-17 01:26:27 +0530

    Easy Wood Cutter

---
+++ Unit 1/PR 101  Assignment - 3 (Conditional Statements & Logical Operator)/Easy Wood Cutter.java
@@ -0,0 +1,10 @@
+// https://oj.masaischool.com/contest/10066/problem/02
+
+  public static void divisibleBy3(int n){
+    //write your code here
+    if (n%3==0){
+        System.out.println("Yes");
+    }else{
+        System.out.println("No");
+    }
+  }
