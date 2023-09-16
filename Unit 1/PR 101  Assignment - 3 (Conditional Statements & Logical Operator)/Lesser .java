3e67b791c4337b0aed07519fcc9d4ed32dcfbc81
ParveendThakur <parveend1234@gmail.com>
2023-09-17 01:30:08 +0530

    Lesser

---
+++ Unit 1/PR 101  Assignment - 3 (Conditional Statements & Logical Operator)/Lesser .java
@@ -0,0 +1,10 @@
+// https://oj.masaischool.com/contest/10066/problem/02_optional_2
+
+public static void smallerOrNot(int x, int y){
+  //write your code here
+  if (x<y){
+      System.out.println("Lesser");
+  }else{
+      System.out.println("Not Lesser");
+  }
+}
