7dcbcf7808690267f2fb2b2c90bf2b4fe073f2a6
ParveendThakur <parveend1234@gmail.com>
2023-09-17 00:58:12 +0530

    Equal to masai

---
+++ Unit 1/PR 101  Assignment - 3 (Conditional Statements & Logical Operator)/Equal to masai.java
@@ -0,0 +1,11 @@
+// https://oj.masaischool.com/contest/10066/problem/00_optional_2
+
+public static void equalToMasai(String str){
+  //write your code here
+  
+  if (str.equals("masai")){
+      System.out.println("Yes");
+  }else{
+      System.out.println("No");
+  }
+}
