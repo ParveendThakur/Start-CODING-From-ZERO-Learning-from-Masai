c584c88934216120f3540ee313bd3e2100b2f171
ParveendThakur <parveend1234@gmail.com>
2023-09-17 00:46:32 +0530

    Equal to 2

---
+++ Unit 1/PR 101  Assignment - 3 (Conditional Statements & Logical Operator)/Equal to 2.java
@@ -0,0 +1,10 @@
+// https://oj.masaischool.com/contest/10066/problem/00_optional_1
+
+public static void equalToTwo(int n){
+  //write your code here
+  if (n==2){
+      System.out.println("Yes");
+  }else{
+      System.out.println("No");
+  }
+}
