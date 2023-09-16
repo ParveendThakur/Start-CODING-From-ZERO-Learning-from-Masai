6f1baf7331d485ec6d92bf71b0a242b665801b65
ParveendThakur <parveend1234@gmail.com>
2023-09-16 21:35:01 +0530

    asic Calculator

---
+++ Unit 1/Assignment 2 - Mathematical & Comparison Operator/Set 2 Writing Java Programs.java
@@ -0,0 +1,27 @@
+// https://masai-curriculum-framework.notion.site/Assignment-1-PR101-Introduction-to-Java-9e73f4a616bd4668a3043701baeaa0e3
+
+/*
+# Assignment 1 - PR101 - Introduction to Java
+
+## Write the Java program for the following problem statements :
+
+- Write the code on replit
+- Submit the replit link
+
+1. **Basic Calculator:**
+    
+    Create a Java program that acts as a simple calculator. It should take in two numerical inputs and an operator (+, -, *, /). Based on these inputs, it should perform the operation and print the result. Handle the possible exceptions like division by zero.
+    
+2. **Temperature Converter:**
+    
+    Write a Java program that converts temperatures between Fahrenheit and Celsius. The program should ask the user to input a temperature and specify whether it's in Fahrenheit or Celsius. Then, it should convert the temperature to the other unit.
+    
+3. **BMI Calculator:**
+    
+    Implement a Body Mass Index (BMI) calculator in Java. The program should take height (in meters) and weight (in kilograms) as input, then calculate and print the BMI using the formula: **`BMI = weight (kg) / (height (m))^2`**
+
+*/
+
+
+// https://replit.com/@parveend1234/Assignment-2-Mathematical-and-Comparison-Operator#BasicCalculator.java
+
