0b9b2803ffed1dfae521b299e5c98de25f195b5c
ParveendThakur <parveend1234@gmail.com>
2023-09-17 01:28:19 +0530

    Greater

---
+++ Unit 1/PR 101  Assignment - 3 (Conditional Statements & Logical Operator)/Greater .java
@@ -0,0 +1,10 @@
+// https://oj.masaischool.com/contest/10066/problem/02_optional_1
+
+public static void greaterOrNot(int x, int y){
+  //write your code here
+  if(x>y){
+      System.out.println("Greater");
+  }else{
+      System.out.println("Not Greater");
+  }
+}
