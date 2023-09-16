96031f0d35097909707f278d4db2de2fa38015d7
ParveendThakur <parveend1234@gmail.com>
2023-09-17 01:19:00 +0530

    Divisible by 4

---
+++ Unit 1/PR 101  Assignment - 3 (Conditional Statements & Logical Operator)/Divisible by 4.java
@@ -0,0 +1,10 @@
+// https://oj.masaischool.com/contest/10066/problem/01
+
+  public static void divisibleBy4(int n){
+    //write your code here
+    if (n%4==0){
+        System.out.println("Yes");
+    }else{
+        System.out.println("No");
+    }
+  }
