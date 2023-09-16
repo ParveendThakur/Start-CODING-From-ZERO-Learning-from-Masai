c43481f2d0f0d2ef75eb8fd17e2a5aa12a37fb42
ParveendThakur <parveend1234@gmail.com>
2023-09-17 01:45:39 +0530

    Masai Even or Odd

---
+++ Unit 1/PR 101  Assignment - 3 (Conditional Statements & Logical Operator)/Masai Even or Odd.java
@@ -0,0 +1,10 @@
+// https://oj.masaischool.com/contest/10066/problem/04
+
+public static void evenOrNot(int n){
+  //write your code here
+  if (n%2==0){
+      System.out.println("Even");
+  }else{
+      System.out.println("Odd");
+  }
+}
