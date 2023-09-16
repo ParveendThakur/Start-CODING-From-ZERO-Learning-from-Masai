53d40c92d292ea7a5c1676ccd01159c5c51c2da8
ParveendThakur <parveend1234@gmail.com>
2023-09-17 01:35:26 +0530

    The Ashes! (But One day)

---
+++ Unit 1/PR 101  Assignment - 3 (Conditional Statements & Logical Operator)/The Ashes! (But One day).java
@@ -0,0 +1,13 @@
+// https://oj.masaischool.com/contest/10066/problem/03
+
+public static void ashes(int aus, int eng){
+  //write your code here
+  if (aus>eng){
+      System.out.print("Australia");
+  }else if (aus<eng){
+      System.out.print("England");
+  }else if (aus==eng){
+      System.out.print("Tie");
+  }
+  
+}
