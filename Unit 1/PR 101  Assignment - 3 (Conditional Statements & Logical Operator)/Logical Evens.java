4bc24fd5c9382da8ff19626df290b11ca0adc186
ParveendThakur <parveend1234@gmail.com>
2023-09-17 13:38:36 +0530

    Logical Evens

---
+++ Unit 1/PR 101  Assignment - 3 (Conditional Statements & Logical Operator)/Logical Evens.java
@@ -0,0 +1,10 @@
+// https://oj.masaischool.com/contest/10066/problem/16
+
+public static void logicalEven(int one, int two){
+  //write your code here
+  if (one%2==0 && two%2==0){
+      System.out.println("Both");
+  }else{
+      System.out.println("No");
+  }
+}
