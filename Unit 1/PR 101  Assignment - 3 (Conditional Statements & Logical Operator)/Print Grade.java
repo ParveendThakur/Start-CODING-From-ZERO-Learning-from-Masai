8fe1a48626dacad39c255f023e0117e6023ee07a
ParveendThakur <parveend1234@gmail.com>
2023-09-17 12:31:34 +0530

    Print Grade

---
+++ Unit 1/PR 101  Assignment - 3 (Conditional Statements & Logical Operator)/Print Grade.java
@@ -0,0 +1,13 @@
+// https://oj.masaischool.com/contest/10066/problem/08
+
+public static void findGrade(int total){
+  //write your code here
+  if (total==100){
+      System.out.println("A");
+  }else if (total>=90){
+      System.out.println("B");
+  }else if (total>=80){
+      System.out.println("C");
+  }else 
+  System.out.println("F");
+}
