8df37ebccc6a18333056e0eae3bd008541c61100
ParveendThakur <parveend1234@gmail.com>
2023-09-17 13:12:19 +0530

    Compare Four Numbers

---
+++ Unit 1/PR 101  Assignment - 3 (Conditional Statements & Logical Operator)/Compare Four Numbers.java
@@ -0,0 +1,10 @@
+// https://oj.masaischool.com/contest/10066/problem/11
+
+public static void compareFourNumber(int one, int two, int three, int four){
+  //write your code here
+  if (one > two && three > four){
+      System.out.println("Yes");
+  }else {
+      System.out.println("No");
+  }
+}
