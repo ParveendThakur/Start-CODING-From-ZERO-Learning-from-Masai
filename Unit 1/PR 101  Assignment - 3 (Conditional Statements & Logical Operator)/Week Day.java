5d02b0596d14923ea6ab225c760fb345493923d1
ParveendThakur <parveend1234@gmail.com>
2023-09-17 13:56:42 +0530

    Week Day

---
+++ Unit 1/PR 101  Assignment - 3 (Conditional Statements & Logical Operator)/Week Day.java
@@ -0,0 +1,20 @@
+// https://oj.masaischool.com/contest/10066/problem/18
+
+public static void weekDay(int num){
+  //write your code here
+  if (num == 1){
+      System.out.print("Monday");
+  }else if (num == 2){
+      System.out.print("Tuesday");
+  }else if (num == 3){
+      System.out.print("Wednesday");
+  }else if (num == 4){
+      System.out.print("Thursday");
+  }else if (num == 5){
+      System.out.print("Friday");
+  }else if (num == 6){
+      System.out.print("Saturday");
+  }else if (num == 7){
+      System.out.print("Sunday");
+  }
+}
