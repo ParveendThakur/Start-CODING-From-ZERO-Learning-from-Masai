faecca32e761741f019d764ea8c0fe43d7723ff7
ParveendThakur <parveend1234@gmail.com>
2023-09-17 14:11:37 +0530

    Mod of 10

---
+++ Unit 1/PR 101  Assignment - 3 (Conditional Statements & Logical Operator)/Mod of 10.java
@@ -0,0 +1,28 @@
+// https://oj.masaischool.com/contest/10066/problem/19
+
+public static void modOf10(int x){
+  //write your code here
+  x = x%10;
+  
+  if (x==0){
+      System.out.print("zero");
+  }else if (x==1){
+      System.out.print("one");
+  }else if (x==2){
+      System.out.print("two");
+  }else if (x==3){
+      System.out.print("three");
+  }else if (x==4){
+      System.out.print("four");
+  }else if (x==5){
+      System.out.print("five");
+  }else if (x==6){
+      System.out.print("six");
+  }else if (x==7){
+      System.out.print("seven");
+  }else if (x==8){
+      System.out.print("eight");
+  }else if (x==9){
+      System.out.print("nine");
+  }
+}
