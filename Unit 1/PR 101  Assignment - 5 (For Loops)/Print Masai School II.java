e31b88bf4f87dbe6b24a0e5a7221622ff18d45ac
ParveendThakur <parveend1234@gmail.com>
2023-09-17 15:26:01 +0530

    Print Masai School II

--- Unit 1/PR 101  Assignment - 5 (For Loops)/Print Masai School II.java
+++ Unit 1/PR 101  Assignment - 5 (For Loops)/Print Masai School II.java
@@ -1,4 +1,13 @@
 // https://course.masaischool.com/assignments/31480
 // https://oj.masaischool.com/contest/10085/problems
 // 
-// 
+// https://oj.masaischool.com/contest/10085/problem/01
+
+  public static void printMasaiSchool(int num){
+    //write your code here
+    
+    
+    for (int i = 0; i < num; i++){
+        System.out.println("Masai School");
+    }
+  }
