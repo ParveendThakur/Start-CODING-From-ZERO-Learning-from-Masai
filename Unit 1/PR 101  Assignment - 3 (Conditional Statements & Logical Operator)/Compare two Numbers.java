ed6f41104f2575b768adb736742f6c12f9b9e123
ParveendThakur <parveend1234@gmail.com>
2023-09-17 12:58:54 +0530

    Compare two Numbers

---
+++ Unit 1/PR 101  Assignment - 3 (Conditional Statements & Logical Operator)/Compare two Numbers.java
@@ -0,0 +1,21 @@
+// https://oj.masaischool.com/contest/10066/problem/10
+
+public static void compareTwoNumber(int num1, int num2){
+  //write your code here
+  if (num1 > num2){
+      System.out.println("true");
+  }else {
+      System.out.println("false");
+  }
+  if(num1<num2){
+      System.out.println("true");
+  }else {
+      System.out.println("false");
+  }
+  
+  if(num1==num2){
+      System.out.println("true");
+  }else {
+      System.out.println("false");
+  }
+}
