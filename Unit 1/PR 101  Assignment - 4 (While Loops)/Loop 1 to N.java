b15a7ebd02e0dc484a47270e989ae6df73bd1b98
ParveendThakur <parveend1234@gmail.com>
2023-09-17 14:45:35 +0530

    Loop 1 to N

---
+++ Unit 1/PR 101  Assignment - 4 (While Loops)/Loop 1 to N.java
@@ -0,0 +1,10 @@
+// https://oj.masaischool.com/contest/10075/problem/02
+
+  public static void loop1toN(int num){
+    //write your code here
+    int i = 1;
+    while (i <= num){
+        System.out.println(i);
+        i++;
+    }
+  }
