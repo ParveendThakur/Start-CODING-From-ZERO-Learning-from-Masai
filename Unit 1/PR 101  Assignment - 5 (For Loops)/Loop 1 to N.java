678dcb1ef262abcb75a4604a962ddde4deaf1473
ParveendThakur <parveend1234@gmail.com>
2023-09-18 13:50:34 +0530

    Loop 1 to N

---
+++ Unit 1/PR 101  Assignment - 5 (For Loops)/Loop 1 to N.java
@@ -0,0 +1,8 @@
+// https://oj.masaischool.com/contest/10085/problem/02
+
+  public static void loop1toN(int num){
+    //write your code here
+    for (int i = 1; i <= num; i++){
+        System.out.println(i);
+    }
+  }
