c810351d4087bd0011e517cb2c091951583678d5
ParveendThakur <parveend1234@gmail.com>
2023-09-18 13:58:55 +0530

    From A to B

---
+++ Unit 1/PR 101  Assignment - 5 (For Loops)/From A to B.java
@@ -0,0 +1,9 @@
+// https://oj.masaischool.com/contest/10085/problem/03
+
+  public static void fromAtoB(int max, int min){
+    //write your code here
+   // int x = min;
+    for (int x=min; x<max; x++){
+        System.out.println(x);
+    }
+  }
