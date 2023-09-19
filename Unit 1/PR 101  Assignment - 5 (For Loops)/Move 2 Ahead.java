fb62758a58a0e7a2e21d89c674d53651016c8dfb
ParveendThakur <parveend1234@gmail.com>
2023-09-19 21:15:09 +0530

    Move 2 Ahead

---
+++ Unit 1/PR 101  Assignment - 5 (For Loops)/Move 2 Ahead.java
@@ -0,0 +1,10 @@
+// https://oj.masaischool.com/contest/10085/problem/05
+
+  public static void moveTwoAhead(int num){
+    //write your code here
+    
+    for (int i = 1; i<=num; i++){
+        if(i%2!=0)
+        System.out.println(i);
+    }
+  }
