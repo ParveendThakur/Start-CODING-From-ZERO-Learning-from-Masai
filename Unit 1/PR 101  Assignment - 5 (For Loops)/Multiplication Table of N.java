993ab5f3bc67ece9508eeed5ac61912f5055ee12
ParveendThakur <parveend1234@gmail.com>
2023-09-19 21:07:07 +0530

    Multiplication Table of N

---
+++ Unit 1/PR 101  Assignment - 5 (For Loops)/Multiplication Table of N.java
@@ -0,0 +1,9 @@
+// https://oj.masaischool.com/contest/10085/problem/04
+
+  public static void multiplicationTableN(int num){
+    //write your code here
+    for (int i=1; i <=10; i++){
+        System.out.println(num*i);
+    }
+    
+  }
