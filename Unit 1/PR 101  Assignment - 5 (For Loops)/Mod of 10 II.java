191cdc2a0de2a6b575428f62316501f3615a334b
ParveendThakur <parveend1234@gmail.com>
2023-09-19 22:22:17 +0530

    Mod of 10 II

---
+++ Unit 1/PR 101  Assignment - 5 (For Loops)/Mod of 10 II.java
@@ -0,0 +1,10 @@
+// https://oj.masaischool.com/contest/10085/problem/09
+
+  public static void modeof10(int n){
+    //write your code here
+    int x = 0;
+    for (int i = 1; i<= n; i++){
+        x = i%10;
+        System.out.println(x);
+    }
+  }
