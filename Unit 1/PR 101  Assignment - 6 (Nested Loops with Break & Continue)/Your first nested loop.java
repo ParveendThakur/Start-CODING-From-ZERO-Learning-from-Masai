be5b50811069c2561113638a367c9245b882714c
ParveendThakur <parveend1234@gmail.com>
2023-09-22 23:56:33 +0530

    Your first nested loop

--- Unit 1/PR 101  Assignment - 6 (Nested Loops with Break & Continue)/Your first nested loop.java
+++ Unit 1/PR 101  Assignment - 6 (Nested Loops with Break & Continue)/Your first nested loop.java
@@ -2,3 +2,14 @@
 // https://oj.masaischool.com/contest/10087?password=87128
 // https://oj.masaischool.com/contest/10087/problems
  // https://oj.masaischool.com/contest/10087/problem/01
+
+
+   public static void yourFirstNested(int num){
+    //write your code here
+    
+    for (int i = 1 ; i <= num ; i++){
+        for (int j = 1; j <= num; j++ ){
+            System.out.print(j +" ");
+        }System.out.println();
+    }
+  }
