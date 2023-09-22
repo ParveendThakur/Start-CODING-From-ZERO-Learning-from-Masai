4ca320c523d774f381c704b9e9bcac85f1b8a7be
ParveendThakur <parveend1234@gmail.com>
2023-09-22 23:52:05 +0530

    Cohort-88 W2-D4 B29 Contest-5 [Nested Loops with Break/Continue]

--- Unit 1/PR 101  Assignment - 6 (Nested Loops with Break & Continue)/Your first nested loop.java
+++ Unit 1/PR 101  Assignment - 6 (Nested Loops with Break & Continue)/Your first nested loop.java
@@ -1,2 +1,4 @@
 // https://students.masaischool.com/assignments/31494?tab=assignmentDetails
 // https://oj.masaischool.com/contest/10087?password=87128
+// https://oj.masaischool.com/contest/10087/problems
+ // https://oj.masaischool.com/contest/10087/problem/01
