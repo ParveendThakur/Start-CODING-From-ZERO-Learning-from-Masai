1f1c6d38c1f7fee913deb411ac19e4853c52b0bb
ParveendThakur <parveend1234@gmail.com>
2023-09-29 23:05:07 +0530

    Your First Pattern

--- Unit 1/PR 101  Assignment - 6 (Nested Loops with Break & Continue)/Your First Pattern.java
+++ Unit 1/PR 101  Assignment - 6 (Nested Loops with Break & Continue)/Your First Pattern.java
@@ -1,2 +1,11 @@
 // https://oj.masaischool.com/contest/10087/problem/02
 
+  public static void yourFirstPattern(int num){
+    //write your code here
+    
+    for (int i = 0; i <num; i++){
+        for (int j = 0; j<num; j++){
+            System.out.print("*" + " ");
+        }System.out.println();
+    }
+  }
