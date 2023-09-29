cf5958c212d15d8568171fae53a61c4c80ae4241
ParveendThakur <parveend1234@gmail.com>
2023-09-29 23:11:52 +0530

    Another Nested Loop

---
+++ Unit 1/PR 101  Assignment - 6 (Nested Loops with Break & Continue)/Another Nested Loop.java
@@ -0,0 +1,11 @@
+// https://oj.masaischool.com/contest/10087/problem/03
+
+  public static void anotherNested(int num){
+    //write your code here
+    for (int i =1; i <=num; i++){
+        for (int j=1; j<=i; j++){
+            System.out.println(j);
+        }
+    }
+  }
+
